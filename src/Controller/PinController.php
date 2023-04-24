16714aef78e394dbfbde2f80baa4ce48c6e7d49c
juldblack <juliendunia@hotmail.com>
2023-04-24 15:18:23 +0200

    Création du projet pinterest dev avec webapp

---
+++ src/Controller/PinController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PinController extends AbstractController
+{
+    #[Route('/pin', name: 'app_pin')]
+    public function index(): Response
+    {
+        // return $this->render('pin/index.html.twig', [
+        //     'controller_name' => 'PinController',
+        // ]);
+        // return new Response("Hello World !!!");
+        // return $this->json(['message' => 'Hello World']);
+        return $this->render('pin/index.html.twig');
+    }
+}
