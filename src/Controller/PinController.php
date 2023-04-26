6c921d1b00d5622db520d8720a52d67f0ef83797
juldblack <juliendunia@hotmail.com>
2023-04-26 08:55:46 +0200

    Ajout de createdAt et updatedAt dans mon entity PIN + la migration en BD

--- src/Controller/PinController.php
+++ src/Controller/PinController.php
@@ -6,9 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Pin;
+
 class PinController extends AbstractController
 {
-    #[Route('/pin', name: 'app_pin')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         // return $this->render('pin/index.html.twig', [
@@ -16,6 +18,18 @@ class PinController extends AbstractController
         // ]);
         // return new Response("Hello World !!!");
         // return $this->json(['message' => 'Hello World']);
+        // $pin = new Pin;
+        // var_dump($pin);
+        // die;
+
+        $pin = new Pin;
+        dump($pin);  //(mieux que le var_dump, PRINCIPALEMENT UTILISER)
+        // die;
+
+        // $pin = new Pin;
+        // dd($pin);  //   (DUMP DIE, meilleur version du dump, plus besoin de mettre le die)
+
+
         return $this->render('pin/index.html.twig');
     }
 }
---
+++ src/Entity/Pin.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PinRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PinRepository::class)]
+#[ORM\Table(name: "pins")]
+class Pin
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
